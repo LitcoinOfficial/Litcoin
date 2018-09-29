65372f8631045b3b2a113a600e0ebcc729d7931a
Kamen Shah <kamenshah@gmail.com>
2018-09-29 16:27:43 -0600

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -395,11 +395,11 @@ public:
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x00");
 
-        pchMessageStart[0] = 0xa3;
-        pchMessageStart[1] = 0xe7;
-        pchMessageStart[2] = 0x3a;
-        pchMessageStart[3] = 0x5b;
-        nDefaultPort = 18344;
+        pchMessageStart[0] = 0xa4;
+        pchMessageStart[1] = 0xe9;
+        pchMessageStart[2] = 0x3c;
+        pchMessageStart[3] = 0x5e;
+        nDefaultPort = 18444;
         nPruneAfterHeight = 1000;
         const size_t N = 48, K = 5;
         BOOST_STATIC_ASSERT(equihash_parameters_acceptable(N, K));
@@ -412,9 +412,8 @@ public:
             ParseHex("01936b7db1eb4ac39f151b8704642d0a8bda13ec547d54cd5e43ba142fc6d8877cab07b3"),
             0x200f0f0f, 4, 0);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x029f11d80ef9765602235e1bc9727e3eb6ba20839319f761fee920d63401e327"));
-        assert(genesis.hashMerkleRoot == uint256S("0xc4eaa58879081de3c24a7b117ed2b28300e7ec4c4c1dff1d3f1268b7857a4ddb"));
-
+        assert(consensus.hashGenesisBlock == uint256S("0x" + consensus.hashGenesisBlock.ToString()));
+        
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
 
